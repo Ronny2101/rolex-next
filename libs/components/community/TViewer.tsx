55e66c1a5c96b18942de18f74bf3fe12a9175c32
Ronny2101 <dilshoddaminov2121@gmail.com>
2026-05-31 22:41:33 +0900

    fix: BRRR

--- libs/components/community/TViewer.tsx
+++ libs/components/community/TViewer.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+//@ts-ignore
 import '@toast-ui/editor/dist/toastui-editor.css';
 import { Viewer } from '@toast-ui/react-editor';
 import { Box, Stack, CircularProgress } from '@mui/material';
